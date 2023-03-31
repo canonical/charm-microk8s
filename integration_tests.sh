53018f99f62a6abca3526871f754fdad22f45ef2
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2023-03-31 20:40:11 +0300

    fix invocation of integration tests

--- integration_tests.sh
+++ integration_tests.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -ex
 
-# specify charmhub name or local path. Set to `build` to build charm from source
-export MK8S_CHARM=microk8s
+# specify charm name to deploy. examples:
+#   'ch:microk8s'         -- install charm 'microk8s' from CharmHub.
+#   './microk8s.charm'    -- install charm from local path.
+#   'build'               -- build charm from source before testing.
+export MK8S_CHARM="ch:microk8s"
+# specify channel to install microk8s charm from.
+export MK8S_CHARM_CHANNEL=edge
 # test for multiple snap versions, e.g. '1.21 1.22 1.23' (space separated list)
-export MK8S_SNAP_CHANNELS=''
+export MK8S_SNAP_CHANNELS='1.26 1.25 1.24 1.23'
 # size of cluster to create during the tests
 export MK8S_CLUSTER_SIZE=3
 # machine constraints for the MicroK8s cluster (passed directly to Juju)
