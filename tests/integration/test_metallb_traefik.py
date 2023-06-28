b020abd8b565b9d6885b293ec1d6d40bbd968d85
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2023-06-28 13:55:32 +0300

    Traefik integration test (#85)

--- tests/integration/test_metallb_traefik.py
+++ tests/integration/test_metallb_traefik.py
@@ -15,7 +15,7 @@ LOG = logging.getLogger(__name__)
 
 
 @pytest.mark.abort_on_fail
-async def test_metallb(e: OpsTest):
+async def test_metallb_traefik(e: OpsTest):
     # deploy microk8s
     if "microk8s" not in e.model.applications:
         await e.model.deploy(
@@ -27,9 +27,13 @@ async def test_metallb(e: OpsTest):
         )
         await e.model.wait_for_idle(["microk8s"])
 
+    u: Unit = e.model.applications["microk8s"].units[0]
+    ns = ""
+
     # bootstrap a juju cloud on the deployed microk8s
     async with microk8s_kubernetes_cloud_and_model(e, "microk8s") as k8s_model:
-        with e.model_context(k8s_model):
+        with e.model_context(k8s_model) as model:
+            ns = model.info.name
             LOG.info("Deploy MetalLB")
             await e.model.deploy(
                 config.MK8S_METALLB_SPEAKER_CHARM,
@@ -41,17 +45,28 @@ async def test_metallb(e: OpsTest):
                 config={"iprange": "42.42.42.42-42.42.42.42"},
             )
             await e.model.wait_for_idle(["metallb-speaker", "metallb-controller"])
-
-        LOG.info("Create a test LoadBalancer service")
-        u: Unit = e.model.applications["microk8s"].units[0]
-
-        await run_unit(u, "microk8s kubectl delete service nginx")
-        await run_unit(u, "microk8s kubectl create deploy nginx --replicas 3 --image nginx")
-        await run_unit(u, "microk8s kubectl expose deploy nginx --port 80 --type LoadBalancer")
+            LOG.info("Deploy Traefik")
+            await e.model.deploy(
+                config.MK8S_TRAEFIK_K8S_CHARM,
+                application_name="traefik",
+                trust=True,
+            )
+            LOG.info("Deploy hello kubecon")
+            await e.model.deploy(
+                config.MK8S_HELLO_KUBECON_CHARM,
+                application_name="hello-kubecon",
+            )
+            await e.model.wait_for_idle(["traefik", "hello-kubecon"])
+            await e.model.relate("traefik", "hello-kubecon")
 
         stdout = ""
         while "42.42.42.42" not in stdout:
-            rc, stdout, stderr = await run_unit(u, "microk8s kubectl get svc nginx")
-            LOG.info("Check LoadBalancer service %s", (rc, stdout, stderr))
+            rc, stdout, stderr = await run_unit(u, f"microk8s kubectl get svc traefik -n {ns}")
+            LOG.info("Check LoadBalancer service %s on %s", (rc, stdout, stderr), ns)
 
-        LOG.info("LoadBalancer successfully got ExternalIP address")
+        # Make sure hello-kubecon is available from ingress
+        while "Hello, Kubecon" not in stdout:
+            rc, stdout, stderr = await run_unit(
+                u, f"curl http://42.42.42.42:80/{ns}-hello-kubecon {ns}"
+            )
+            LOG.info("Check LoadBalancer service %s on %s", (rc, stdout, stderr), ns)
