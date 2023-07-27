99224fe4116142aeb3ae9f657a49256da1d90c23
Sachin Kumar Singh <sachin.singh@canonical.com>
2023-07-27 15:01:48 +0530

    debugging again

--- tests/integration/test_microk8s.py
+++ tests/integration/test_microk8s.py
@@ -70,7 +70,7 @@ async def test_microk8s_cluster(e: OpsTest, series: str, cp_units: int, worker_u
             f"{application_name}:workers", f"{application_name}-worker:control-plane"
         )
 
-        await e.model.wait_for_idle(f"{application_name}-worker", timeout=60 * 60)
+        await e.model.wait_for_idle([f"{application_name}-worker"], timeout=60 * 60)
 
     await e.model.wait_for_idle([application_name], timeout=60 * 60)
 
