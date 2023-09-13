4828a1393423618d086176b409b558ccc819ef60
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2023-09-13 17:42:57 +0300

    extend unit test for workers as well

--- tests/unit/test_regression.py
+++ tests/unit/test_regression.py
@@ -6,7 +6,7 @@ from conftest import Environment
 from ops.model import BlockedStatus
 
 
-@pytest.mark.parametrize("role", ["", "control-plane"])
+@pytest.mark.parametrize("role", ["", "control-plane", "worker"])
 def test_regression_charm_remains_blocked_on_invalid_containerd_registries_config_after_join(
     e: Environment, role: str
 ):
@@ -17,8 +17,14 @@ def test_regression_charm_remains_blocked_on_invalid_containerd_registries_confi
     e.harness.begin_with_initial_hooks()
     assert isinstance(e.harness.charm.unit.status, BlockedStatus)
 
-    prel_id = e.harness.charm.model.get_relation("peer").id
-    e.harness.update_relation_data(prel_id, e.harness.charm.app.name, {"join_url": "join"})
+    if role == "worker":
+        rel_id = e.harness.add_relation("control-plane", "microk8s-cp")
+        e.harness.add_relation_unit(rel_id, "microk8s-cp/0")
+        e.harness.update_relation_data(rel_id, "microk8s-cp", {"join_url": "fakejoinurl"})
+    else:
+        prel_id = e.harness.charm.model.get_relation("peer").id
+        e.harness.update_relation_data(prel_id, e.harness.charm.app.name, {"join_url": "join"})
 
+    e.microk8s.join.assert_called_once()
     assert e.harness.charm._state.joined
     assert isinstance(e.harness.charm.unit.status, BlockedStatus)
